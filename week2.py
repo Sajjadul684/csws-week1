aedff3c3cf9294de4c2ef304268ccc5e77849d25
Mohammed Sajjadul <M.sajjadul@2020.ljmu.ac.uk>
2022-03-29 12:12:01 +0100

    adding names onto end of list

--- week2.py
+++ week2.py
@@ -4,3 +4,6 @@ greetings = ["Hello By", "Good Evening", "Danke"]
 print (greetings [0] + " " + names [0])
 print (greetings [1] + " " + names [1])
 print (greetings [2] + " " + names [2])
+
+names.append ("Samuel")
+print (names) #Adds name onto end of list
