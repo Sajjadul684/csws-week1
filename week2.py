b6f0f0c6e7f040c99dccaba14be51c0b5e23f0d9
Mohammed Sajjadul <M.sajjadul@2020.ljmu.ac.uk>
2022-03-29 12:19:51 +0100

    list commands

--- week2.py
+++ week2.py
@@ -7,3 +7,11 @@ print (greetings [2] + " " + names [2])
 
 names.append ("Samuel")
 print (names) #Adds name onto end of list
+
+names.insert(3, "Liam") #inserting via element
+
+del names[1] #deleting name
+
+names.remove("Samuel") #removing via element
+
+x = names.pop(3) #reusing deleted element
